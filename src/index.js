fd372c8d4d00b81aeff42117ff082a740d9d5d19
KanoCode <kanokanombola@gmail.com>
2022-04-27 12:37:10 +0200

    Update index.js, Add list items dynamically

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import "./style.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-const tdoItems = [
-    {description:"Wash the dishes",
-completed:false,
-index:0}
-,{description:"Complete To Do list Project",
-completed:false,
-index:1}
-]
+const toDoList = [
+  { description: "Wash the dishes", completed: false, index: 0 },
+  { description: "Complete To Do list Project", completed: false, index: 1 },
+];
 
-const toDoList = document.getElementById("to-do-list");
+const ToDoList = document.getElementById("to-do-list");
 
-console.log(toDoList)
- 
+console.log(toDoList);
+const createTodoItem = (obj) => {
+  const tdoItem = document.createElement("div");
+  tdoItem.className = "tdo-item";
+  tdoItem.innerHTML = ` <span class="before"></span>
+<input
+  class="${obj.completed ? "completed" : ""}"
+  type="button"
+  value="${obj.description}"
+  onclick="msg()"
+/>
+
+<i class="fa-solid fa-ellipsis-vertical"></i>
+`;
+  ToDoList.append(tdoItem);
+};
+
+toDoList.forEach((a, i) => {
+  createTodoItem(a);
+  console.log(i, a.index);
+});
