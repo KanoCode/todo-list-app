96a9399ff28ded15efd2eb52294356cfee6d8e39
KanoCode <kanokanombola@gmail.com>
2022-04-27 11:49:12 +0200

    Add styles for to do list, Update package.json

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./style.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
 
+const tdoItems = [
+    {description:"Wash the dishes",
+completed:false,
+index:0}
+,{description:"Complete To Do list Project",
+completed:false,
+index:1}
+]
+
+const toDoList = document.getElementById("to-do-list");
+
+console.log(toDoList)
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,18 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  entry: {
+    home: "./src/index.js",
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: ["file-loader"],
       },
     ],
   },
@@ -16,4 +23,7 @@ module.exports = {
       template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
+  devServer: {
+    // liveReload: true,
+  },
 };
