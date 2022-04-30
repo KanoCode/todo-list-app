7832fcfe41b5011437a9e9e8374329f3a27f1769
KanoCode <kanokanombola@gmail.com>
2022-04-30 08:24:53 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
-import "./style.css";
-import "./modules/dependencies";
-import "./modules/UpdateList";
-import * as lo from "lodash";
-import { ToDoList } from "./modules/createList";
-import removeAll from "./modules/removeAll";
+import './style.css';
+import './modules/dependencies';
+import './modules/UpdateList';
+import * as lo from 'lodash';
+import { ToDoList } from './modules/createList';
+import removeAll from './modules/removeAll';
 
-import reloadList from "./modules/preload";
+import reloadList from './modules/preload';
 
-const toDoList = JSON.parse(localStorage.getItem("activityArr"));
+const toDoList = JSON.parse(localStorage.getItem('activityArr'));
 
 const todoItems = ToDoList.childNodes;
 
 reloadList(toDoList);
 
-const allToDoInputs = document.querySelectorAll("#to-do-list .tdo-item");
+const allToDoInputs = document.querySelectorAll('#to-do-list .tdo-item');
 
 todoItems.forEach((node, i) => {
   const input = node.childNodes[3];
   const deleteBtns = node.childNodes[5];
-  deleteBtns.addEventListener("click", () => {
-    if (input.value === "") {
+  deleteBtns.addEventListener('click', () => {
+    if (input.value === '') {
       const filterdDoList = lo.without(toDoList, toDoList[i]);
       filterdDoList.map((obj, i) => {
         obj.index = i;
         return obj;
       });
       reloadList(filterdDoList);
-      localStorage.setItem("activityArr", JSON.stringify(filterdDoList));
+      localStorage.setItem('activityArr', JSON.stringify(filterdDoList));
       window.location.reload();
     } else {
       window.location.reload();
     }
   });
 
-  if (node.classList.contains("completed")) {
+  if (node.classList.contains('completed')) {
     node.childNodes[1].innerHTML = '<i class="fa-solid fa-check"></i>';
   }
 
   // input events form editing
-  input.addEventListener("click", () => {
-    const lists = document.querySelectorAll(".to-do-list input[type=text]");
+  input.addEventListener('click', () => {
+    const lists = document.querySelectorAll('.to-do-list input[type=text]');
     lists.forEach((item) => {
-      item.type = "button";
+      item.type = 'button';
     });
-    input.type = "text";
+    input.type = 'text';
 
     input.addEventListener(
-      "keypress",
+      'keypress',
       (e) => {
-        if (e.key === "Enter") {
-          if (input.value === "") {
+        if (e.key === 'Enter') {
+          if (input.value === '') {
             const filterdDoList = lo.without(toDoList, toDoList[i]);
             reloadList(filterdDoList);
 
@@ -58,19 +58,19 @@ todoItems.forEach((node, i) => {
               return obj;
             });
 
-            localStorage.setItem("activityArr", JSON.stringify(filterdDoList));
+            localStorage.setItem('activityArr', JSON.stringify(filterdDoList));
             window.location.reload();
           } else {
-            const newList = JSON.parse(localStorage.getItem("activityArr"));
+            const newList = JSON.parse(localStorage.getItem('activityArr'));
 
             newList[i].description = input.value;
 
-            localStorage.setItem("activityArr", JSON.stringify(newList));
+            localStorage.setItem('activityArr', JSON.stringify(newList));
             window.location.reload();
           }
         }
       },
-      false
+      false,
     );
   });
 });
@@ -78,14 +78,14 @@ todoItems.forEach((node, i) => {
 // make active class
 allToDoInputs.forEach((a, i, arr) => {
   const copyArr = Array.from(arr);
-  a.addEventListener("click", (e) => {
+  a.addEventListener('click', (e) => {
     const filterd = copyArr.filter((a) => a.id !== e.target.id);
 
     filterd.forEach((a) => {
-      a.classList.remove("active");
+      a.classList.remove('active');
     });
     const clicked = lo.difference(copyArr, filterd)[0];
-    clicked.className = "active tdo-item";
+    clicked.className = 'active tdo-item';
 
     const toNodeList = function (arrayOfNodes) {
       const fragment = document.createDocumentFragment();
@@ -100,27 +100,27 @@ allToDoInputs.forEach((a, i, arr) => {
 });
 
 // checkboxes
-const checkboxes = document.querySelectorAll(".before");
+const checkboxes = document.querySelectorAll('.before');
 
 checkboxes.forEach((a, i) => {
-  a.addEventListener("click", () => {
-    if (a.parentElement.classList.contains("completed")) {
-      a.parentElement.classList.remove("completed");
-      const currentLocalList = JSON.parse(localStorage.getItem("activityArr"));
+  a.addEventListener('click', () => {
+    if (a.parentElement.classList.contains('completed')) {
+      a.parentElement.classList.remove('completed');
+      const currentLocalList = JSON.parse(localStorage.getItem('activityArr'));
       currentLocalList[i].completed = false;
-      localStorage.setItem("activityArr", JSON.stringify(currentLocalList));
-      a.innerHTML = "";
+      localStorage.setItem('activityArr', JSON.stringify(currentLocalList));
+      a.innerHTML = '';
     } else {
-      a.parentElement.classList.add("completed");
+      a.parentElement.classList.add('completed');
       a.innerHTML = '<i class="fa-solid fa-check"></i>';
-      const currentLocalList = JSON.parse(localStorage.getItem("activityArr"));
+      const currentLocalList = JSON.parse(localStorage.getItem('activityArr'));
       currentLocalList[i].completed = true;
-      localStorage.setItem("activityArr", JSON.stringify(currentLocalList));
+      localStorage.setItem('activityArr', JSON.stringify(currentLocalList));
       window.location.reload();
     }
   });
 });
 
 // remove all completed items
-const removeCompleted = document.querySelector(".last-row");
-removeCompleted.addEventListener("click", removeAll);
+const removeCompleted = document.querySelector('.last-row');
+removeCompleted.addEventListener('click', removeAll);
